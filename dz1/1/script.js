98559116fa53a71fcf167168b7f82ee4fc07deed
Elmira7 <90276363+Elmira7@users.noreply.github.com>
2024-06-01 21:41:05 +0300

    add task 1

---
+++ dz1/1/script.js
@@ -0,0 +1,11 @@
+document.getElementById('button1').addEventListener('click', function() {
+    console.log('я первая кнопка');
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+    console.log('я вторая кнопка');
+});
+
+document.getElementById('button3').addEventListener('click', function() {
+    console.log('я третья кнопка');
+});
