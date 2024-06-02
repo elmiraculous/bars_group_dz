abd2eafbac7d0539d82668d98e5349f4e061c1c7
Elmira7 <90276363+Elmira7@users.noreply.github.com>
2024-06-02 17:46:16 +0300

    add task2

---
+++ dz1/2/script.js
@@ -0,0 +1,13 @@
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+document.getElementById('button1').addEventListener('click', function() {
+    document.body.style.backgroundColor = getRandomColor();
+})
